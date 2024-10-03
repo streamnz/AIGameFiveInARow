88af9fdea14aee6015093029dc421014922eb5fc
Hao <hao.streamnz@gmail.com>
2024-10-03 13:00:19 +0800

    调整navbar的样式

--- frontend/src/component/Navbar.js
+++ frontend/src/component/Navbar.js
@@ -1,8 +1,7 @@
-// src/component/Navbar.js
 import React from 'react';
 import './Navbar.css';  // 引入样式文件
 
-function Navbar({ loggedInUser, onLoginClick, onRegisterClick }) {
+function Navbar({ loggedInUser, onLoginClick, onRegisterClick, onLogoutClick }) {
   return (
     <div className="navbar">
       <div className="navbar-left">
@@ -11,16 +10,17 @@ function Navbar({ loggedInUser, onLoginClick, onRegisterClick }) {
 
       <div className="navbar-right">
         {loggedInUser ? (
-          // 如果用户已登录，显示用户名和设置
+          // 如果用户已登录，显示 Welcome 和 Settings
           <div className="user-info">
             <span>Welcome, {loggedInUser.username}!</span>
-            <button className="settings-btn">Settings</button>
+            <button className="navbar-btn settings-btn">Settings</button>
+            <button className="navbar-btn logout-btn" onClick={onLogoutClick}>Logout</button>
           </div>
         ) : (
           // 如果用户未登录，显示 Login 和 Register
           <div className="auth-buttons">
-            <button className="login-btn" onClick={onLoginClick}>Login</button>
-            <button className="register-btn" onClick={onRegisterClick}>Register</button>
+            <button className="navbar-btn login-btn" onClick={onLoginClick}>Login</button>
+            <button className="navbar-btn register-btn" onClick={onRegisterClick}>Register</button>
           </div>
         )}
       </div>
