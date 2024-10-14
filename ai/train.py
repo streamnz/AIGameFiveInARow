c1967c98a0ce55c2ce9e793bc293105f0ea3e8ad
Hao <hao.streamnz@gmail.com>
2024-10-15 02:17:40 +0530

    change args of train model

--- ai/train.py
+++ ai/train.py
@@ -28,23 +28,23 @@ class TrainPipeline():
                            n_in_row=self.n_in_row)
         self.game = Game(self.board)
         # training params
-        self.learn_rate = 1e-2
+        self.learn_rate = 2e-3
         self.lr_multiplier = 1.0  # adaptively adjust the learning rate based on KL
         self.temp = 1.0  # the temperature param
         self.n_playout = 400  # num of simulations for each move
         self.c_puct = 5
         self.buffer_size = 10000
-        self.batch_size = 128  # mini-batch size for training
+        self.batch_size = 512  # mini-batch size for training
         self.data_buffer = deque(maxlen=self.buffer_size)
         self.play_batch_size = 1
-        self.epochs = 2  # num of train_steps for each update
+        self.epochs = 5  # num of train_steps for each update
         self.kl_targ = 0.02
-        self.check_freq = 20
-        self.game_batch_num = 100
+        self.check_freq = 50
+        self.game_batch_num = 1500
         self.best_win_ratio = 0.0
         # num of simulations used for the pure mcts, which is used as
         # the opponent to evaluate the trained policy
-        self.pure_mcts_playout_num = 100
+        self.pure_mcts_playout_num = 1000
 
         # 根据棋盘长宽和n_in_row动态生成模型文件名
         self.model_filename = f'best_policy_{self.board_width}_{self.board_height}_{self.n_in_row}.model2'
