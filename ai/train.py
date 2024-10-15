57c3415e93f2f475308e1776e8e61fce64919154
Hao <hao.streamnz@gmail.com>
2024-10-15 07:42:42 +0530

    change dic of train model*2

--- ai/train.py
+++ ai/train.py
@@ -28,17 +28,17 @@ class TrainPipeline():
                            n_in_row=self.n_in_row)
         self.game = Game(self.board)
         # training params
-        self.learn_rate = 2e-3
+        self.learn_rate = 1e-3
         self.lr_multiplier = 1.0  # adaptively adjust the learning rate based on KL
         self.temp = 1.0  # the temperature param
-        self.n_playout = 400  # num of simulations for each move
+        self.n_playout = 800  # num of simulations for each move
         self.c_puct = 5
         self.buffer_size = 10000
-        self.batch_size = 512  # mini-batch size for training
+        self.batch_size = 1024  # mini-batch size for training
         self.data_buffer = deque(maxlen=self.buffer_size)
         self.play_batch_size = 1
-        self.epochs = 5  # num of train_steps for each update
-        self.kl_targ = 0.02
+        self.epochs = 10  # num of train_steps for each update
+        self.kl_targ = 0.05
         self.check_freq = 50
         self.game_batch_num = 1500
         self.best_win_ratio = 0.0
