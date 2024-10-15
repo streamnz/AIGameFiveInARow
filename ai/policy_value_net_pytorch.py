ce5dac0517b265afd236176d984755e26f6ae7cb
Hao <hao.streamnz@gmail.com>
2024-10-15 06:42:42 +0530

    change dic of train model

--- ai/policy_value_net_pytorch.py
+++ ai/policy_value_net_pytorch.py
@@ -62,7 +62,7 @@ class PolicyValueNet():
     """policy-value network """
 
     def __init__(self, board_width, board_height,
-                 model_file=None, use_gpu=False):
+                 model_file=None, use_gpu=True):
         self.use_gpu = use_gpu
         self.board_width = board_width
         self.board_height = board_height
