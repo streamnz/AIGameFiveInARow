6049ff8acd53bfbdb88398d20d75f09df4c832b4
Hao <hao.streamnz@gmail.com>
2024-12-30 16:24:51 +0530

    del ai*2

--- source/utils.py
+++ source/utils.py
@@ -1,65 +1,11 @@
-import random
 import uuid
 
 ##### For managing the interface #####
-SIZE = 540 #size of the board image
-PIECE = 32 #size of the single pieces
+SIZE = 540  # size of the board image
+PIECE = 32  # size of the single pieces
 N = 15
 MARGIN = 23
-GRID = (SIZE - 2 * MARGIN) / (N-1)
-
-def pixel_conversion(list_points, target):
-    # point of the list from where start the search
-    index = int((len(list_points)-1)//2) 
-
-    while True:
-        if target < list_points[0]:
-            index = 0
-            break
-        elif target >= list_points[-1]:
-            index = len(list_points)-2
-            break
-
-        elif list_points[index] > target:
-            if list_points[index-1] <= target:
-                index -= 1
-                break
-            else:
-                index -= 1
-
-        elif list_points[index] <= target:
-            if list_points[index+1] > target:
-                break
-            else:
-                index += 1
-    
-    return index
-
-
-# Transform pygame pixel to boardMap coordinates
-def pos_pixel2map(x, y):
-    start = int(MARGIN - GRID//2)
-    end = int(SIZE - MARGIN + GRID//2)
-    list_points = [p for p in range(start, end+1, int(GRID))]
-
-    i = pixel_conversion(list_points, y)
-    j = pixel_conversion(list_points, x)
-    return (i,j)
-
-# Transform boardMap to pygame pixel coordinates
-def pos_map2pixel(i, j):
-    return (MARGIN + j * GRID - PIECE/2, MARGIN + i * GRID - PIECE/2)
-
-
-def create_mapping():
-    pos_mapping = {}
-    for i in range(N):
-        for j in range(N):
-            spacing = [r for r in range(MARGIN, SIZE-MARGIN+1, int(GRID))]
-            pos_mapping[(i,j)] = (spacing[j],spacing[i])
-    
-    return pos_mapping
-
+GRID = (SIZE - 2 * MARGIN) / (N - 1)
 
 
 #### Pattern scores ####
@@ -119,12 +65,12 @@ def create_pattern_dict():
     return patternDict
 
 
-
 ##### Zobrist Hashing #####
 def init_zobrist():
-    zTable = [[[uuid.uuid4().int  for _ in range(2)] \
-                        for j in range(15)] for i in range(15)] #changed to 32 from 64
+    zTable = [[[uuid.uuid4().int for _ in range(2)] \
+               for j in range(15)] for i in range(15)]  # changed to 32 from 64
     return zTable
+
 
 def update_TTable(table, hash, score, depth):
     table[hash] = [score, depth]
