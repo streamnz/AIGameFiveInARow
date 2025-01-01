40686ebb45e78da305882e050b20ddc9e8deb39b
Hao <hao.streamnz@gmail.com>
2025-01-01 11:02:53 +0530

    add 10 second time limit of ai_move

--- source/gomoku.py
+++ source/gomoku.py
@@ -1,17 +1,29 @@
-from source.AI import *
+import math
+import time
+
+from multiprocessing import Process, Queue
 
 
-def ai_move(ai):
+def ai_move_logic(ai, queue):
     ai.depth = 3
     ai.alphaBetaPruning(ai.depth, ai.boardValue, ai.nextBound, -math.inf, math.inf, True)
     if ai.isValid(ai.currentI, ai.currentJ):
-        move_i, move_j = ai.currentI, ai.currentJ
-        ai.updateBound(move_i, move_j, ai.nextBound)
+        queue.put((ai.currentI, ai.currentJ))
     else:
         ai.updateBound(ai.currentI, ai.currentJ, ai.nextBound)
         bound_sorted = sorted(ai.nextBound.items(), key=lambda el: el[1], reverse=True)
         pos = bound_sorted[0][0]
-        move_i = pos[0]
-        move_j = pos[1]
-        ai.currentI, ai.currentJ = move_i, move_j
-    return move_i, move_j
+        queue.put((pos[0], pos[1]))
+
+
+
+def ai_move(ai, timeout=10):
+    queue = Queue()
+    process = Process(target=ai_move_logic, args=(ai, queue))
+    process.start()
+    process.join(timeout)
+    if process.is_alive():
+        print("AI move timed out, returning default move.")
+        process.terminate()
+        return 7, 7
+    return queue.get()
