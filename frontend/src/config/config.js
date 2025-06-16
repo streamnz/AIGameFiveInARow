3b54a89b0a51cb420c36b2dcfdfc7e4150110eac
hao <hao.streamnz@gmail.com>
2025-06-17 01:07:40 +1200

    socket cors 1

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -9,7 +9,7 @@ const config = {
   // 生产环境配置
   production: {
     API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'https://www.streamnz.com/api',
-    SOCKET_URL: process.env.REACT_APP_SOCKET_URL || 'https://www.streamnz.com/api',
+    SOCKET_URL: process.env.REACT_APP_SOCKET_URL || 'https://streamnz-api.streamnz.com',
     IS_PRODUCTION: true,
   }
 };
